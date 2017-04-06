8dd4fdd15c6053bf672605f078fc5b954361f929
Erj4 <edward1999.jones@gmail.com>
2017-04-06 12:57:43 +0100

    I'm your slave

--- Maze.java
+++ Maze.java
@@ -81,7 +81,7 @@ public class Maze{
                 grid[i][j]=0;
             }
         }
-        final int numberOfRooms=512;
+        final int numberOfRooms=1024;
         //numberOfRooms=num;
         List<Room> rooms=new ArrayList<>();
         System.out.println("Making rooms...");
